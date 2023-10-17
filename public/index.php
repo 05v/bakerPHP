acd7e7868f309ac430481f1adbd127c140b28066
vladverheij <vladverheij@gmail.com>
2023-10-17 09:46:13 +0200

    add assets to structure

--- public/index.php
+++ public/index.php
@@ -1,36 +1,53 @@
-<?php
-/**
- * Display all errors
- */
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+<!DOCTYPE html>
+<html lang="en">
 
-/**
- * Test a mail
- */
-$mailTo = 'persoon@nieuwsbrief.nl';
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/style/style.css">
+    <script src="../assets/javascript/main.js" defer></script>
+</head>
 
-// Define the headers so we can use HTML
-$headers  = "From: afzender@testmail.nl\r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+<body>
+    <?php
+    /**
+     * Display all errors
+     */
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 
-// define the subject
-$subject = 'Voorbeeld test mail';
+    /**
+     * Test a mail
+     */
+    $mailTo = 'persoon@nieuwsbrief.nl';
 
-// define the html content
-$html = 'hoi tester,<br>' . "\n\r";
-$html .= 'Dit is een voorbeeld mail van mij.<br>' . "\n\r";
-$html .= 'En hier staat een link: <a href="https://www.w3schools.com/php/func_mail_mail.asp">Mail voorbeeld</a><br>' . "\n\r";
-$html .= 'groetjes,<br>' . "\n\r";
-$html .= 'De test mail server';
+    // Define the headers so we can use HTML
+    $headers  = "From: afzender@testmail.nl\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
-// send the mail using php mail 
-$res = mail($mailTo, $subject, $html, $headers);
+    // define the subject
+    $subject = 'Voorbeeld test mail';
 
-/**
- * Display the result
- */
-echo 'is de mail verstuurd? ';
-var_dump( $res);
+    // define the html content
+    $html = 'hoi tester,<br>' . "\n\r";
+    $html .= 'Dit is een voorbeeld mail van mij.<br>' . "\n\r";
+    $html .= 'En hier staat een link: <a href="https://www.w3schools.com/php/func_mail_mail.asp">Mail voorbeeld</a><br>' . "\n\r";
+    $html .= 'groetjes,<br>' . "\n\r";
+    $html .= 'De test mail server';
+
+    // send the mail using php mail 
+    $res = mail($mailTo, $subject, $html, $headers);
+
+    /**
+     * Display the result
+     */
+    echo 'is de mail verstuurd? ';
+    var_dump($res);
+    ?>
+
+</body>
+
+</html>
